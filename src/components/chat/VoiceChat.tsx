12342d07c6dfc9b30fb6240418b0ea353255d369
Raven Jiang <raven@cs.stanford.edu>
2020-06-03 23:18:03 -0700

    Avoid using variable name client for AudioClient

--- src/components/chat/VoiceChat.tsx
+++ src/components/chat/VoiceChat.tsx
@@ -15,31 +15,31 @@ type Props = {
 
 const VoiceChat = (props: Props) => {
   const classes = useStyles({ theme: useTheme() });
-  const [client, setClient] = React.useState<AudioClient | null>(null);
+  const [audio, setAudio] = React.useState<AudioClient | null>(null);
   const [connected, setConnected] = React.useState<boolean>(false);
   const [muted, setMuted] = React.useState<boolean>(false);
   const [loading, setLoading] = React.useState<boolean>(false);
   const { actorId, sam } = props;
 
   React.useEffect(() => {
-    const newClient = new AudioClient(sam, props.client.joinAudio);
-    setClient(newClient);
+    const newAudio = new AudioClient(sam, props.client.joinAudio);
+    setAudio(newAudio);
   }, [props.client, sam]);
 
-  if (!client) {
+  if (!audio) {
     return <div className={classes.root} />;
   }
 
   const handleJoin = () => {
     setLoading(true);
-    client.join(actorId, props.client.roomId).then(() => {
+    audio.join(actorId, props.client.roomId).then(() => {
       setConnected(true);
       setLoading(false);
     });
   };
 
   const handleExit = () => {
-    client.leave().then(() => {
+    audio.leave().then(() => {
       setMuted(false);
       setConnected(false);
     });
@@ -47,7 +47,7 @@ const VoiceChat = (props: Props) => {
 
   const handleMute = () => {
     let m = !muted;
-    client.mute(m);
+    audio.mute(m);
     setMuted(m);
   };
 
